fe9679aee1646bc5b9a342b2f857afed18311aed
heartape <64122416+heartape@users.noreply.github.com>
2023-09-13 18:06:16 +0800

    shell-8086

---
+++ shell-8086/boot/bootsect.asm
@@ -0,0 +1,53 @@
+start:
+	mov ax,0x07c0
+	mov ds,ax
+	mov ax,0x9000
+	mov es,ax
+	mov cx,256
+	sub si,si
+	sub di,di
+	rep movw
+
+	jmpi go,0x9000
+;迁移引导扇区
+go: 
+	mov ax,cs
+    mov ds,ax
+    mov es,ax
+    mov ss,ax
+    mov sp,0xFF00
+
+;读取引导扇区后的4个扇区
+load_setup:
+    mov dx,0x0000   ; dh:磁头,dl:驱动器
+    mov cx,0x0002   ; ch:磁道,cl:扇区号
+    mov bx,0x0200   ; es:bx指向读入数据的内存地址，前面共有512个字节
+    mov ax,0x0204   ; ah=2表示读，al:扇区数
+    int 0x13
+    jnc ok_load_setup
+    mov dx,0x0000
+    mov ax,0x0000
+    int 0x13
+    jmp load_setup
+
+ok_load_setup:
+    mov ax,0x1000
+    mov es,ax
+
+load_system:
+    mov dx,0x0000
+    mov cx,0x0006
+    mov bx,0x0a00
+    mov ax,0x02f0
+    int 0x13
+    jnc ok_load_system
+    mov dx,0x0000
+    mov ax,0x0000
+    int 0x13
+    jmp load_system
+
+ok_load_system:
+    jmpi 0,0x9020
+
+  	times 510 - ($ - $$) db 0
+    db 55h, 0aah
